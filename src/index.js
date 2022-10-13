4ed721bc09ecb4eda2a86c3bac1c3b86b61440bf
aliciams <# apt-get install git>
2022-10-13 11:00:22 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 //import 'react-bootstrap';
 
 ReactDOM.render(
